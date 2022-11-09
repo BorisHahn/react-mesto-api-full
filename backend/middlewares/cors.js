735cd4f58e1d37b4506327ed423a6f8463008f1f
Boris Gaidukevich <Borishahn@yandex.ru>
2022-11-09 19:36:48 +0300

    0.0.9

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-  res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Origin', '*');
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
--- backend/utils/const.js
+++ backend/utils/const.js
@@ -1,11 +1,9 @@
 module.exports.created = 200;
 module.exports.URL_REG = /(https?:\/\/)(w{3}\.)?(((\d{1,3}\.){3}\d{1,3})|((\w-?)+\.))(:\d{2,5})?((\/.+)+)?\/?#?/;
 module.exports.allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
   'http://localhost:3000',
   'http://localhost:3001',
+  'http://gaidukevich.mesto.nomoredomains.icu',
   'https://gaidukevich.mesto.nomoredomains.icu',
-  'https://api.gaidukevich.mesto.nomoredomains.icu',
 ];
 module.exports.DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
