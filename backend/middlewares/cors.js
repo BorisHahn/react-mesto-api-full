49c06d8875e285b69b76b5004748113a2e957b01
Boris Gaidukevich <Borishahn@yandex.ru>
2022-11-09 21:23:17 +0300

    1.0.2

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -4,11 +4,10 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  res.header('Access-Control-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-  // res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
